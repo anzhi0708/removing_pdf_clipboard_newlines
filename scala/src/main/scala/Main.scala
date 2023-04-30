cdf4e5ac560b5032e8dbd808d72824e38aa4c99c
Anji <anzhi0708@gmail.com>
2023-04-30 21:24:35 +0900

    add .scalafmt.conf

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -10,21 +10,14 @@ def getClipboardBuffer: String = result("pbpaste" :: Nil)
 
 // Copy a string to clipboard.
 def copyToClipboard(toCopy: String): Int =
-  val string = toCopy.replace("\"", "\\\"").replace("'", "\'")
+  val string  = toCopy.replace("\"", "\\\"").replace("'", "\'")
   val payload = s"""echo "$string" | pbcopy"""
-  val cmd = "bash" :: "-c" :: payload :: Nil
+  val cmd     = "bash" :: "-c" :: payload :: Nil
   Process(cmd).run().exitValue()
 
 def removeNewLines(str: String): String =
   val pattern: Regex = "\n([a-zA-Z\"])".r
   pattern.replaceAllIn(str, m => s" ${m.group(1)}")
-
-/* These were nonfunctional. Incorrect approach
-def escapeQuotes(str: String): String =
-  str.replace("'", "__SINGLE_QUOTE__").replace("\"", "__DOUBLE_QUOTE__")
-def restoreQuotes(str: String): String =
-  str.replace("__SINGLE_QUOTE__", "'").replace("__DOUBLE_QUOTE__", "\"")
-*/
 
 @main def hello: Unit =
   println("Hello world!")
@@ -32,7 +25,15 @@ def restoreQuotes(str: String): String =
   while true do {
     Thread.sleep(1000)
     val textWithoutNewLine = removeNewLines(getClipboardBuffer)
-    val _ = copyToClipboard(textWithoutNewLine)
+    val _                  = copyToClipboard(textWithoutNewLine)
   }
 
 def msg = "I was compiled by Scala 3. :)"
+
+/* These were nonfunctional, incorrect approach
+def escapeQuotes(str: String): String =
+  str.replace("'", "__SINGLE_QUOTE__").replace("\"", "__DOUBLE_QUOTE__")
+def restoreQuotes(str: String): String =
+  str.replace("__SINGLE_QUOTE__", "'").replace("__DOUBLE_QUOTE__", "\"")
+ */
+
