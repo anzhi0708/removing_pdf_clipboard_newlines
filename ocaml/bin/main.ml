c5a9ddede45e71f99d87bfe1218563de0ed2a7c2
Anji <anzhi0708@gmail.com>
2023-04-30 21:34:55 +0900

    remove unnecessary lines in OCaml implementation

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,25 +1,5 @@
-let color_grey : string = "\027[1;30m"
-let color_reset : string = "\027[0m"
-let grey (s : string) : string = color_grey ^ s ^ color_reset
-let animation : string list = [ "-"; "\\"; "|"; "/" ]
-
-let refresh (frame : int ref) : string =
-  frame := !frame + 1;
-  if !frame = 4 then
-    let () = frame := 0 in
-    List.nth animation 0
-  else List.nth animation !frame
-
-(*
-let escape (s : string) (origin : string) (replacement : string) =
-  (* Replace every {origin} with {replacement}. *)
-  let regex = Str.regexp origin in
-  let result = Str.global_replace regex replacement s in
-  result
-*)
-
+(* Run a command and get the console output *)
 let result (cmd : string) : string =
-  (* Run a command and get the console output *)
   let fd = Unix.open_process_in cmd in
   let text = In_channel.input_all fd in
   let _ = Unix.close_process_in fd in
@@ -37,12 +17,8 @@ let remove_newlines (str : string) : string =
   Str.global_replace regex " \\1" str
 
 let main () : unit =
-  let frame_begin : int ref = ref 0 in
+  let () = Printf.printf "Running...%!" in
   while true do
-    let () =
-      Printf.printf "%s%!"
-        ("\r" ^ grey (refresh frame_begin) ^ grey " Running...")
-    in
     let () = Unix.sleep 1 in
     let text_without_newline =
       get_clipboard_buffer () |> remove_newlines |> String.trim
